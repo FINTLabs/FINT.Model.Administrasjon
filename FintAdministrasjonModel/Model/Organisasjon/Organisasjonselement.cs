a670c00a4eece6e186f0fdd54f815c1a5dc5dc8a
Ole Anders Eidjord <oae@vaf.no>
2017-03-30 15:18:36 +0200

    Oppdatet i henhold til gjeldende modell

---
+++ FintAdministrasjonModel/Model/Organisasjon/Organisasjonselement.cs
@@ -0,0 +1,19 @@
+﻿using FintFellesModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintAdministrasjonModel.Model.Organisasjon
+{
+    public class Organisasjonselement : Enhet
+    {
+        public string Navn { get; set; }
+        public string Kortnavn { get; set; }
+        public bool Aktiv { get; set; }
+        public Identifikator OrganisasjonsId { get; set; }
+        public Identifikator OrganisasjonsKode { get; set; }
+        public Periode Gyldighetsperiode { get; set; }
+    }
+}
---
+++ FintAdministrasjonModel/Model/Personal/Personalressurs.cs
@@ -0,0 +1,18 @@
+﻿using FintFellesModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintAdministrasjonModel.Model.Personal
+{
+    public class Personalressurs
+    {
+        public Identifikator brukernavn { get; set; }
+        public Identifikator systemId { get; set; }
+        public Identifikator ansattnummer { get; set; }
+        public Periode ansettelsesperiode { get; set; }
+        public Kontaktinformasjon kontaktinformasjon { get; set; }
+    }
+}
